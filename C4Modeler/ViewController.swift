eb40098359b0fb7cfbdec72aed5056e5ca7fb866
rollmind <eyerama@gmail.com>
2019-02-23 21:58:17 +0900

    Initial Commit

---
+++ C4Modeler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C4Modeler
+//
+//  Created by eyebasket on 23/02/2019.
+//  Copyright © 2019 rollmind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ C4ModelerUITests/C4ModelerUITests.swift
@@ -0,0 +1,34 @@
+//
+//  C4ModelerUITests.swift
+//  C4ModelerUITests
+//
+//  Created by eyebasket on 23/02/2019.
+//  Copyright © 2019 rollmind. All rights reserved.
+//
+
+import XCTest
+
+class C4ModelerUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
